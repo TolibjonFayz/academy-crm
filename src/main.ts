41b325ae06f8a8de94765ecadae83932b3d4229e
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2024-03-11 13:02:40 +0500

    Lets start

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 3300;
+
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+
+    const config = new DocumentBuilder()
+      .setTitle('Academy')
+      .setDescription('A project named ERP by Tolibjon')
+      .setVersion('1.0.0')
+      .addTag('NestJS, Postgres, Sequelize, Vuejs, TailwindCss')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
